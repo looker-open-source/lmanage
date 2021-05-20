5fd8fccfd87fe6b70efdc59d7f6c5ec5ddb6fecb
hselbie <hselbie@gmail.com>
2021-05-20 16:38:15 +0000

    stashing larger changes in favor of code cleanliness

--- lmanage/cli.py
+++ lmanage/cli.py
@@ -11,15 +11,6 @@ def lmanage():
     pass
 
 
-# @lmanage.command()
-# @click.option("-d", "--days", help="How many days has the user to be removed been inactive for")
-# @click.option("-i", "--ini-file", help="Path to the ini file to use for sdk authentication")
-# @click.option("-rm", "--removeuser", help="disable user in instance", default=False)
-# @click.option("--csv", help="export your delinquent user into a csv", default=False)
-# def removeuser(**kwargs):
-#     delinquent_user.main(**kwargs)
-
-
 @lmanage.command()
 @click.option("-fp", "--path",
               type=click.Path(exists=True),
@@ -34,20 +25,3 @@ def lmanage():
               help="Add a fully scoped fieldname (e.g. view_name.field_name) to return a csv with these values")
 def mapview(**kwargs):
     get_content_with_views.main(**kwargs)
-
-
-# @lmanage.command()
-# @click.option("-d", "--days", help="How many days has the user to be removed been inactive for")
-# @click.option("-i", "--ini-file", help="Path to the ini file to use for sdk authentication")
-# @click.option("-b", "--backup-location", help="file path where you would like backed up content to live")
-# def dcontent(**kwargs):
-#     delinquent_content.main(**kwargs)
-
-
-# @lmanage.command()
-# @click.option("-bd", "--broken-dash-id", help="Helper content to send out unique message related to content")
-# @click.option("-bl", "--broken-look-id", help="Helper content to send out unique message related to content")
-# @click.option("-i", "--ini-file", help="Path to the ini file to use for sdk authentication")
-# @click.option("-b", "--backup-url", help="gzr instance that you should input for back up (the machine name in your .netrc file)")
-# def broke_content(**kwargs):
-#     validate_content.main(**kwargs)
