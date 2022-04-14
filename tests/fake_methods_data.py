d414a0de76555a8ae789ef57e961a50a2835beb7
hselbie <hselbie@gmail.com>
2022-04-14 18:25:21 +0000

    stuff

--- tests/fake_methods_data.py
+++ tests/fake_methods_data.py
@@ -1,29 +1,79 @@
-input_data = {'role_BusinessOperations_Developer':
-              {'role': 'BODevelopers',
-               'permissions': ['access_data', 'use_sql_runner'],
-               'model_set': [{'name': 'lk1_test_set', 'models': ['test', 'test2']}],
-               'team': ['BusinessOperations_BO_Dev']},
-              'folder_permissions':
-              {'business_operations_folder': [
-                  {'name': 'Business Operations',
-                   'team_view': ['Snaptest'],
-                   'subfolder': [{'name': 'test_sub', 'team_edit': ['Freddy'], 'team_view': ['hugo']},
-                                 {'name': 'test_sub2',
-                                  'subfolder':
-                                  [{'name': 'test_sub_sub',
-                                    'team_edit': ['Famke'],
-                                    'team_view': ['hugle']},
-                                   {'name': 'subdiddy',
-                                    'subfolder': [{'name': 'hugle_testy', 'team_edit': ['Freddy'], 'team_view': ['hugle']}]}]}]}],
+input_data = {
+    'permission_sets': {
+        'developer': {
+            'permissions': ['access_data', 'can_see_system_activity', 'see_dashboards', 'clear_cache_refresh', 'create_table_calculations', 'deploy', 'develop', 'download_without_limit', 'explore', 'manage_spaces', 'mobile_app_access', 'save_content', 'schedule_look_emails', 'see_drill_overlay', 'see_lookml', 'see_sql', 'see_user_dashboards', 'send_to_integration', 'use_sql_runner']},
+        'user': {
+            'permissions': ['access_data', 'clear_cache_refresh', 'create_table_calculations', 'download_without_limit', 'explore', 'manage_spaces', 'mobile_app_access', 'save_content', 'schedule_look_emails', 'see_drill_overlay', 'see_lookml', 'see_lookml_dashboards', 'see_looks', 'see_sql', 'see_user_dashboards', 'send_to_integration']}},
+    'model_sets': {
+        'lk1_test_set': {
+            'models': ['test', 'test2']},
+        'lk4_huggy': {
+            'models': ['test', 'test1', 'test2']}},
+    'roles': {
+        'BusinessOperations_Developer': {
+            'permissions_set': 'test_perm1',
+            'model_set': 'developer',
+            'team': ['BusinessOperations_BO_Dev', 'DrewEdit']},
 
-               'ua_region_all':
-               {'name': 'region_all',
-                'type': 'string',
-                'hidden_value': 'false',
-                'user_view': 'true',
-                'user_edit': 'false',
-                'value': ['us', 'ag', 'bb', 'dd'],
-                'team': ['Cameos', 'Freddy', 'AudreyGroup']}}}
+        'BusinessOperations_User': {
+            'permissions': 'user',
+            'model_set': 'lk4_huggy',
+            'team': ['BusinessOperations', 'Freddy']}},
+    'folder_permissions': {
+        'business_operations_folder': [{
+            'name': 'Business Operations',
+            'team_view': ['BusinessOperations', 'Snaptest'],
+            'subfolder': [{
+                'name': 'test_sub',
+                'team_edit': ['Freddy'],
+                'team_view': ['hugo']},
+                {'name': 'test_sub2',
+                 'subfolder': [{
+                               'name': 'test_sub_sub',
+                               'team_edit': ['Famke'],
+                               'team_view': ['hugle']},
+                               {'name': 'subdiddy',
+                                'subfolder': [{
+                                              'name': 'hugle_testy',
+                                              'team_edit': ['Freddy'],
+                                              'team_view': ['hugle']}]}]}]}],
+        'sexy_time_folder': [{
+            'name': 'sexy time',
+            'team_edit': ['sexy_group1'],
+            'team_view': ['sexy_group2'],
+            'subfolder': [{
+                'name': 'sexy_sub'}]}],
+        'suffering_succotash_folder': [{
+            'name': 'suffering succotash',
+            'team_edit': ['sexy_group1'],
+            'team_view': ['sexy_group2'],
+            'subfolder': [{
+                'name': 'another_one',
+                'team_edit': ['sexy_group1', 'new_group'],
+                'team_view': ['newer_group']}]}]},
+    'user_attributes': {
+        'region_all': {
+            'type': 'string',
+            'hidden_value': 'false',
+            'user_view': 'true',
+            'user_edit': 'false',
+            'value': ['us', 'ag', 'bb', 'dd'],
+            'team': ['Cameos', 'Freddy', 'AudreyGroup']},
+        'region_testy': {
+            'type': 'string',
+            'hidden_value': 'false',
+            'user_view': 'true',
+            'user_edit': 'false',
+            'value': ['us', 'ag', 'bb'],
+            'team': ['Cameos', 'AudreyGroup']},
+        'can_see_hugo': {
+            'type': 'string',
+            'hidden_value': 'false',
+            'user_view': 'true',
+            'user_edit': 'false',
+            'value': ['No'],
+            'team': ['Cameos', 'CanSeeDAUGroup']}}}
+
 
 fake_permission_set = [
     {'role_name': 'BODevelopers',
